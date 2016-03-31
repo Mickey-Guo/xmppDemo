0c0d16808c9fb701615eea9d03a4ce838c56f4d9
guoqing <guoqing@xiaokundemacbook-pro.local>
2016-03-31 10:41:27 +0800

    Initial Commit

---
+++ XmppDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XmppDemo
+//
+//  Created by guoqing on 16/3/31.
+//  Copyright © 2016年 guoqing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
