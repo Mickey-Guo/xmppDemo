aa1ffd02ab06b240f6fa9aa3536dacc3feeede24
guoqing <guoqing@xiaokundeMacBook-Pro.local>
2016-04-11 19:47:57 +0800

    Create the project and modify storyboard

---
+++ XmppDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XmppDemo
+//
+//  Created by guoqing on 16/3/31.
+//  Copyright © 2016年 guoqing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
